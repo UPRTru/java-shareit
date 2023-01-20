a7c807676290d03461c6763adad81111ae618ec1
UPRTru <uprt.mi2@yandex.ru>
2023-01-20 05:22:28 +0300

    add-controllers

---
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.item.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<ItemDto> getAll(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemService.getAll(userId);
+    }
+
+    @GetMapping("/{id}")
+    public ItemDto getById(@PathVariable Long id) {
+        return itemService.getById(id);
+    }
+
+    @PostMapping
+    public ItemDto create(@RequestHeader("X-Sharer-User-Id") Long userId, @Valid @RequestBody ItemDto itemDto) {
+        return itemService.create(itemDto, userId);
+    }
+
+    @PatchMapping("/{id}")
+    public ItemDto update(@RequestBody ItemDto itemDto, @PathVariable Long id,
+                          @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemService.update(itemDto, id, userId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        itemService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> search(@RequestParam String text) {
+        return itemService.search(text);
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorageInMemory.java
@@ -0,0 +1,45 @@
+package ru.practicum.shareit.user.storage;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.*;
+
+@Component
+public class UserStorageInMemory implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
+
+    private Long id = 1L;
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return users.get(id) == null ? Optional.empty() : Optional.of(users.get(id));
+    }
+
+    @Override
+    public User create(User user) {
+        user.setId(id);
+        id++;
+        users.put(user.getId(), user);
+
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        users.put(user.getId(), user);
+
+        return user;
+    }
+
+    @Override
+    public void delete(Long id) {
+        users.remove(id);
+    }
+
+}
